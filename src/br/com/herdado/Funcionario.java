bc3c91b141dfebb688a0a89c98c99c44a4667e2b
anailson ribeiro <arsantos315@gmail.com>
2022-09-13 15:43:00 -0300

    utilizando herança

--- src/br/com/herdado/Funcionario.java
+++ src/br/com/herdado/Funcionario.java
@@ -5,31 +5,18 @@ public class Funcionario {
 	private String nome;
 	private String cpf;
 	private double salario;
-	private int tipo = 0; // 0 - funcionario comum , 1 Gerente , 2 Diretor
-
+	
+	
 	public Funcionario() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public double getBonificacao() {
-		
-		if (this.tipo == 0) {//funcionario comum
-			return this.salario * 0.1;
-		}else if(this.tipo == 1) { //Gerente
-			return this.salario ;
-		}else {
-			return this.salario + 1000.0; 
-		}
-		
+
+		return this.salario + 1000.0;
 	}
 
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
-
-	public int getTipo() {
-		return tipo;
-	}
+	
 
 	public String getNome() {
 		return nome;
